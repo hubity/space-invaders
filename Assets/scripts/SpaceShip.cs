52dec8c5b641374d77c91145e6de39ec26c34023
unknown <hubitygames@gmail.com>
2019-03-17 16:54:37 -0300

    add controller player $ sound Manager

---
+++ Assets/scripts/SpaceShip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour {
+
+
+    public float speed = 30;
+
+    private void FixedUpdate()
+    {
+        float horzMove = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horzMove, 0) * speed;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+
+        }
+	}
+}
